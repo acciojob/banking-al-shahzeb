62bf2373d6826cd1ef7c400d1405427902f429fc
al-shahzeb <alishahzeb453@gmail.com>
2023-03-31 01:37:33 +0530

    Banking Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,17 +2,18 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        CurrentAccount currentAccount;
-        try {
-            currentAccount=new CurrentAccount("asd",6576.9,"MOOJOOIO");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            currentAccount.validateLicenseId();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println(currentAccount.getTradeLicenseId());
+//        CurrentAccount currentAccount;
+//        try {
+//            currentAccount=new CurrentAccount("asd",6576.9,"MOOJOOIO");
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//        try {
+//            currentAccount.validateLicenseId();
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//        System.out.println(currentAccount.getTradeLicenseId());
+//    }
     }
 }
